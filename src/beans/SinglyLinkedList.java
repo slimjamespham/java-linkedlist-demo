ed12fe28331b4ac13002b9807f118d58828aac07
Chuong Pham <chuongpham@MacBookPro.local>
2022-07-02 17:31:33 -0500

    Completed insert and delete functions of the linked list.

--- src/beans/SinglyLinkedList.java
+++ src/beans/SinglyLinkedList.java
@@ -4,7 +4,7 @@ package beans;
  */
 class Node {
     //Fields
-    int data;
+    double data;
     Node nextNode;
 
     /*
@@ -19,7 +19,7 @@ class Node {
      * The overloaded constructor
      * @param data The data of a node to set
      */
-    public Node(int data) {
+    public Node(double data) {
         this.data = data;
         this.nextNode = null;
     }
@@ -60,7 +60,7 @@ public class SinglyLinkedList {
      * @param data The element to be inserted
      * @return true if the insert is done successfully
      */
-    public void add(int data) {
+    public void add(double data) {
         Node currentNode = this.headNode;
         if(this.headNode == null) {
             headNode = new Node(data);
@@ -78,7 +78,7 @@ public class SinglyLinkedList {
      * @param data The element to be inserted
      * @return true if the insert is done successfully
      */
-    public void addLast(int data) {
+    public void addLast(double data) {
         this.add(data);
     }
 
@@ -88,7 +88,7 @@ public class SinglyLinkedList {
      * @param index The position to the element to be inserted
      * @param data The element to be inserted
      */
-    public void add(int index, int data) {
+    public void add(int index, double data) {
         if(index < 0 || index >= this.length())
             throw new ArrayIndexOutOfBoundsException("Error: Index is out of range");
             
@@ -114,11 +114,145 @@ public class SinglyLinkedList {
      * the beginning of the list.
      * @param data The element to be inserted
      */
-    public void addFirst(int data) {
+    public void addFirst(double data) {
         Node tempNode = new Node(data);
         tempNode.nextNode = this.headNode;
         this.headNode = tempNode;
     } 
+
+    /*
+     * The method removes the first
+     * element from the linked list
+     * @return true if the list is not empty
+     */
+    public boolean removeFirst() {
+        boolean status = false;
+        if(this.headNode == null) {
+            throw new NullPointerException("Error: cannot remove an " +
+                                            "element from the empty list!");
+        }
+        else {    
+            Node currentNode = this.headNode;    
+            this.headNode = currentNode.nextNode;
+            currentNode.nextNode = null;
+            currentNode = null;
+        }
+        return status;
+    }
+
+    /*
+     * The method removes the last
+     * element from the linked list
+     * @return true if the list is not empty
+     */
+    public boolean removeLast() {
+        boolean status = false;
+        Node currentNode = this.headNode;
+        if(currentNode == null)
+            throw new NullPointerException("Error: cannot remove an " +
+                                            "element from the empty list!");
+        else if(currentNode.nextNode == null) {
+            this.removeFirst();
+            status = true;
+        }
+        else {
+            while(currentNode.nextNode.nextNode != null) {
+                currentNode = currentNode.nextNode;
+            }
+            currentNode.nextNode = null;
+            status = true;
+        }
+        return status;
+    }
+
+    /*
+     * The method removes the element at
+     * the specified position in the list
+     * @param index The position of the element to be deleted
+     * @return true if the list is not empty and the index is valid
+     */
+    public boolean remove(int index) {
+        boolean status = false;
+
+        if(this.headNode == null) 
+            throw new NullPointerException("Error: cannot remove an " +
+                                            "element from the empty list!");
+        
+        else if (index < 0 || index >= this.length()) 
+            throw new ArrayIndexOutOfBoundsException("Error: Index is out of range");
+        
+        else if (index == 0) {
+            removeFirst();
+            status = true;
+        }
+
+        else if(index == this.length() - 1) {
+            removeLast();
+            status = true;
+        }
+        
+        else {
+            Node previousNode = this.headNode;
+            Node currentNode = previousNode.nextNode;
+            int counter = 0;
+            while(counter != (index - 1)) {
+                previousNode = previousNode.nextNode;
+                currentNode = currentNode.nextNode;
+                counter++;
+            }
+            previousNode.nextNode = currentNode.nextNode;
+            currentNode.nextNode = null;
+            currentNode = null;
+            status = true;
+        }
+        return status;
+
+    }
+    
+    /*
+     * The method removes the first occurrence of the 
+     * specified element from the linked list, if it is present
+     * @param data The first occurrence specified element to be deleted
+     * @return true if the element exists in the linked list
+     */
+    public boolean remove(double data) {
+        boolean status = false;
+        
+        if(this.headNode == null) 
+            throw new NullPointerException("Error: cannot remove an " +
+                                            "element from the empty list!");
+
+        //If the specified data is located at the beginning of the list                                    
+        else if(this.headNode.data == data) {
+            removeFirst();
+            status = true;
+        }
+
+        else {
+            Node previousNode = this.headNode;
+            Node currentNode = previousNode.nextNode;
+
+            while(currentNode != null) {
+                if(currentNode.data == data) {
+                    previousNode.nextNode = currentNode.nextNode;
+                    currentNode.nextNode = null;
+                    currentNode = null;
+                    status = true;
+                    break;
+                }
+                previousNode = previousNode.nextNode;
+                currentNode = currentNode.nextNode;
+            }
+        }
+            return status;
+    }
+
+    /*
+     * The method removes all of the elements from the linked list
+     */
+    public void clear() {
+        this.headNode = null;
+    }
 
     /*
      * The method returns the current length of the linked list
